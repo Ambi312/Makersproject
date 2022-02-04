57492b25100a9dc8ccbe72bafb32befa4fded2b6
Ambi312 <abd.kanybek@gmail.com>
2022-02-04 22:45:49 +0600

    final

--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from oursite.views import favourite_add, favourite_list
-from .views import RegisterView, SingInView, profile
+from .views import RegisterView, SingInView, profile, favourite_add, favourite_list
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('login', SingInView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
-    path('login', profile, name='profile'),
+    path('profile', profile, name='profile'),
     path('fav/<int:id>/', favourite_add, name='favourite_add'),
-    path('fav/<int:id>/', favourite_list, name='favourite_list'),
+    path('profile/favourites/', favourite_list, name='favourite_list'),
 ]
