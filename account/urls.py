fbc27ce01afd90fe5d360bddfdc8e55b75a32f19
Iunurov <rashid010196@gmail.com>
2022-02-04 22:43:08 +0600

    padding

--- account/urls.py
+++ account/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('login', SingInView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
-    path('login', profile, name='profile'),
-    path('fav/<int:id>/', favourite_add, name='favourite_add'),
-    path('fav/<int:id>/', favourite_list, name='favourite_list'),
+    path('profile', profile, name='profile'),
 ]
